7094085ebe9aa7fc2b4dc58afcc60e7639e88675
Micaela <micaelacantaro@gmail.com>
2024-09-16 09:50:07 -0300

    lista ordenada

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+
+  const stringElements = array.filter(element => typeof element === 'string');
+  stringElements.sort((a, b) => a.localeCompare(b));
+  
+  stringElements.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+ showList(strangeArray);
+});
